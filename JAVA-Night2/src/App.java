475c9e465e52869aa4687eba5116909f342a083e
isaque hipolito marion silva <qISAQUEp@gmail.com>
2025-02-05 21:24:00 -0300

    Update App.java

--- JAVA-Night2/src/App.java
+++ JAVA-Night2/src/App.java
@@ -1,5 +1,22 @@
+import java.io.FileReader;
+
+import javax.sql.rowset.FilteredRowSet;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // entrada
+        float firstNumber = 25;
+        float secondNumber = 98;
+
+        // processamento
+        float sum = firstNumber + secondNumber;
+        float subtraction = firstNumber - secondNumber;
+        float multiplication = firstNumber * secondNumber;
+        float division = firstNumber / secondNumber;
+
+        //saida
+        System.out.printf("A soma dos números %.0f e %.0f é igual à %.0f",
+                firstNumber, secondNumber, sum);
+
     }
 }
